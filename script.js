196b61da5a34867858959b7f25362fe2ce5b2c3c
ShogunDev <shogundev7@gmail.com>
2026-03-25 10:58:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-// 1. Smooth Scroll avec Lenis
+// 1. SMOOTH SCROLL (LENIS)
 const lenis = new Lenis();
-lenis.on('scroll', ScrollTrigger.update);
-gsap.ticker.add((time) => { lenis.raf(time * 1000); });
-gsap.ticker.lagSmoothing(0);
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
 
-// 2. Animation d'entrée (Text Reveal)
-window.onload = () => {
-    gsap.to(".line span", { translateY: 0, duration: 1.5, stagger: 0.2, ease: "power4.out" });
-};
+// 2. REVEAL HERO TEXT
+gsap.to(".reveal-box span", {
+    translateY: 0,
+    duration: 1.2,
+    ease: "power4.out",
+    delay: 0.5
+});
 
-// 3. Parallaxe sur les images
+// 3. PARALLAXE SUR LES IMAGES DE PROJET
 gsap.utils.toArray('.parallax-img').forEach(img => {
     gsap.to(img, {
-        y: "20%",
+        y: "15%",
         ease: "none",
         scrollTrigger: {
             trigger: img,
@@ -23,15 +28,22 @@ gsap.utils.toArray('.parallax-img').forEach(img => {
     });
 });
 
-// 4. Inversion de couleur des sections (GSAP ScrollTrigger)
-gsap.utils.toArray(".section.light").forEach(section => {
+// 4. CHANGER LA COULEUR DU BODY AU SCROLL
+const panels = gsap.utils.toArray(".panel");
+panels.forEach((panel) => {
     ScrollTrigger.create({
-        trigger: section,
-        start: "top 50%",
-        end: "bottom 50%",
-        onEnter: () => gsap.to("body", { backgroundColor: "#fff", color: "#000", duration: 0.6 }),
-        onLeave: () => gsap.to("body", { backgroundColor: "#000", color: "#fff", duration: 0.6 }),
-        onEnterBack: () => gsap.to("body", { backgroundColor: "#fff", color: "#000", duration: 0.6 }),
-        onLeaveBack: () => gsap.to("body", { backgroundColor: "#000", color: "#fff", duration: 0.6 }),
+        trigger: panel,
+        start: "top 40%",
+        end: "bottom 40%",
+        onEnter: () => updateTheme(panel),
+        onEnterBack: () => updateTheme(panel),
     });
 });
+
+function updateTheme(panel) {
+    if (panel.classList.contains('light')) {
+        gsap.to("body", { backgroundColor: "#fff", color: "#000", duration: 0.5 });
+    } else {
+        gsap.to("body", { backgroundColor: "#050505", color: "#fff", duration: 0.5 });
+    }
+}
